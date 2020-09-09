ade9add9cdfc2b03c97cc67dc1a5e14edaaa3cd8
Michael B Ansong <michael.ansong7@gmail.com>
2020-09-09 23:32:53 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,6 +1,6 @@
-def square_array(array)
-  new_array = []
-  array.each do |array|
+def square_array(numbers)
+  new_numbers = []
+  numbers.each do |array|
   new_array << (num**2)
   end
   return new_array
